bc49e88a6e41a26d19243aa2fb6ba1e7953a2294
eddie <eddiewalsh933@gmail.com>
2022-12-14 11:31:05 +0000

    Constructors with/without images added to classes

--- BusinessEntities/FarmAnimal.cs
+++ BusinessEntities/FarmAnimal.cs
@@ -32,6 +32,11 @@ namespace BusinessEntities
         {
             this.purpose = Purpose;
         }
+        public FarmAnimal(int advertid, string selleremail, string title, string description, double price, bool verified, string status,byte[] imageone, byte[] imagetwo, byte[] imagethree, string animalname, string animaltype, int age, string gender, string purpose)
+        : base(advertid, selleremail, title, description, price, verified, status,imageone,imagetwo,imagethree, animalname, animaltype, age, gender)
+        {
+            this.purpose = Purpose;
+        }
         #endregion
     }
 }
