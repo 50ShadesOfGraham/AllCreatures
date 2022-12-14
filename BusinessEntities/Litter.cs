d78a4fae073e0d72aa0c2267ddf41bafd3fe3579
eddie <eddiewalsh933@gmail.com>
2022-12-14 11:08:21 +0000

    Litter Class Created

--- BusinessEntities/Litter.cs
+++ BusinessEntities/Litter.cs
@@ -14,5 +14,48 @@ namespace BusinessEntities
         private string breedone;
         private string breedtwo;
         #endregion
+        #region Instance Properties
+        public int LitterSize
+        {
+            get { return littersize; }
+            set { littersize = value; }
+        }
+        public bool Purebreed
+        {
+            get { return purebreed; }
+            set { purebreed = value; }
+        }
+        public string BreedOne
+        {
+            get { return breedone; }
+            set { breedone = value; }
+        }
+        public string BreedTwo
+        {
+            get { return breedtwo; }
+            set { breedtwo = value; }
+        }
+        #endregion
+        #region Constructor
+        public Litter()
+        {
+            throw new System.NotImplementedException();
+        }
+        public Litter(int littersize, bool purebreed, string breedone, string breedtwo)
+        {
+            this.littersize = LitterSize;
+            this.purebreed = Purebreed;
+            this.breedone = BreedOne;
+            this.breedtwo = BreedTwo;
+        }
+        public Litter(int advertid, string selleremail, string title, string description, double price, bool verified, string status, string animalname, string animaltype, int age, string gender, int littersize, bool purebreed, string breedone, string breedtwo) 
+            : base(advertid, selleremail, title, description, price, verified, status, animalname, animaltype,age,gender)
+        {
+            this.littersize = LitterSize;
+            this.purebreed = Purebreed;
+            this.breedone = BreedOne;
+            this.breedtwo = BreedTwo;
+        }
+        #endregion
     }
 }
