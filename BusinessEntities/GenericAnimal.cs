4e72128b868f961cf7cee5b4d4e9616b4b3b08c7
eddie <eddiewalsh933@gmail.com>
2023-01-03 15:02:25 +0000

    ddsdf

--- BusinessEntities/GenericAnimal.cs
+++ BusinessEntities/GenericAnimal.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace BusinessEntities
 {
-    internal class GenricAnimal
+    public class GenericAnimal : Animal
     {
+        #region InstanceProperties
+        private string detailone;
+        private string detailtwo;
+        private string detailthree;
+        #endregion
+        #region InstanceProperties
+        public string DetailOne
+        {
+            get { return detailone; }
+            set { detailone = value; }
+        }
+        public string DetailTwo
+        {
+            get { return detailtwo; }
+            set { detailtwo = value; }
+        }
+        public string DetailThree
+        {
+            get { return detailthree; }
+            set { detailthree = value; }
+        }
+        #endregion
+        #region Constructor
+        public GenericAnimal()
+        {
+            throw new System.NotImplementedException();
+        }
+        public GenericAnimal(int advertid, string selleremail, string title, string description, double price, bool verified, string status, byte[] imageone, byte[] imagetwo, byte[] imagethree, string animalname, string animaltype, int age, string gender,string detailone,string detailtwo,string detailthree) 
+            : base(advertid,selleremail,title,description,price,verified,status,imageone,imagetwo,imagethree,animalname,animaltype,age,gender)
+        {
+            this.detailone= detailone;
+            this.detailtwo= detailtwo;
+            this.detailthree= detailthree;
+        }
+        #endregion
     }
 }
