41beff30682a2fa9152daf6b63b3218d23730557
eddie <eddiewalsh933@gmail.com>
2023-01-03 15:12:26 +0000

    Farm Animal and Litter class updated and added!

--- BusinessEntities/FarmAnimal.cs
+++ BusinessEntities/FarmAnimal.cs
@@ -8,6 +8,28 @@ namespace BusinessEntities
 {
     public class FarmAnimal : Animal
     {
-
+        #region Instance Properties
+        private string purpose;
+        #endregion
+        #region Instance Properties
+        public string Purpose
+        {
+            get { return purpose; }
+            set { purpose = value; }
+        }
+        #endregion
+        #region Constructor
+        //With Images
+        public FarmAnimal(int advertid, string selleremail, string title, string description, double price, bool verified, string status, byte[] imageone, byte[] imagetwo, byte[] imagethree, string animalname, string animaltype, int age, string gender, string purpose)
+            : base(advertid, selleremail, title, description, price, verified, status, imageone, imagetwo, imagethree, animalname, animaltype, age, gender)
+        {
+            this.purpose= purpose;
+        }
+        public FarmAnimal(int advertid, string selleremail, string title, string description, double price, bool verified, string status, string animalname, string animaltype, int age, string gender, string purpose)
+            : base(advertid, selleremail, title, description, price, verified, status, animalname, animaltype, age, gender)
+        {
+            this.purpose = purpose;
+        }
+        #endregion
     }
 }
---
+++ BusinessEntities/Litter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class Litter : Animal
+    {
+        #region Instance Properties
+        private int littersize;
+        #endregion
+        #region Instance Properties
+        public int LitterSize
+        {
+            get { return littersize; }
+            set { littersize = value; }
+        }
+        #endregion
+        #region Constructor
+        public Litter(int advertid, string selleremail, string title, string description, double price, bool verified, string status, byte[] imageone, byte[] imagetwo, byte[] imagethree, string animalname, string animaltype, int age, string gender, int littersize)
+            : base(advertid, selleremail, title, description, price, verified, status, imageone, imagetwo, imagethree, animalname, animaltype, age, gender)
+        {
+            this.littersize = littersize;
+        }
+        //without images
+        public Litter(int advertid, string selleremail, string title, string description, double price, bool verified, string status,string animalname, string animaltype, int age, string gender,int littersize)
+            : base(advertid, selleremail, title, description, price, verified, status,animalname, animaltype, age, gender)
+        {
+            this.littersize = littersize;
+        }
+        #endregion
+    }
+}
