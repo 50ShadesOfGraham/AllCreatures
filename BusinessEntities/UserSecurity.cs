8d839ad0a36cbf97e90cebf9368c490a6f04330e
eddie <eddiewalsh933@gmail.com>
2023-01-21 11:55:16 +0000

    User Payment and User Security Class added

---
+++ BusinessEntities/UserSecurity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class UserSecurity : User
+    {
+        #region Instance Properties
+        private string securityquestion;
+        private string securityanswer;
+        #endregion
+        #region Instance Properties
+        public string SecurityQuestion
+        {
+            get { return securityquestion; } 
+            set { securityquestion= value; }
+        }
+        public string SecurityAnswer
+        { 
+            get { return securityanswer; } 
+            set { securityanswer = value; } 
+        }
+        #endregion
+        public UserSecurity() 
+        {
+            throw new System.NotImplementedException();
+        }
+        public UserSecurity(string securityquestion,string securityanswer)
+        {
+            this.securityquestion= securityquestion;
+            this.securityanswer= securityanswer;
+        }
+    }
+}
