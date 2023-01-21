80127687263fe8e838ce71c3b333d292e261e3e5
eddie <eddiewalsh933@gmail.com>
2023-01-21 19:22:46 +0000

    //

---
+++ WindowsClient/NotificationContainer.cs
@@ -0,0 +1,41 @@
+﻿using BusinessEntities;
+using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace WindowsClient
+{
+    public partial class NotificationContainer : Form
+    {
+        private IModel Model;
+        public NotificationContainer(IModel Model)
+        {
+            InitializeComponent();
+            this.Model = Model;
+            this.Text = Model.getUserNameCurrentuser() + " Notifications";
+        }
+
+        private void NotificationContainer_Load(object sender, EventArgs e)
+        {
+            List<Notifications> sortedDates = new List<Notifications>();
+
+            sortedDates = Model.NotificationList.OrderBy(x => x.Messagetime).ToList();
+
+            foreach (Notifications n in sortedDates) 
+            {
+                SingleNotification notif = new SingleNotification(n);
+                notif.Show();
+
+                this.Controls.Add(notif);
+            }
+        }
+    }
+}
---
+++ WindowsClient/SingleNotification.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsClient
+{
+    partial class SingleNotification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SingleNotification));
+            this.TitleLbl = new System.Windows.Forms.Label();
+            this.TrashBttn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TimeLbl = new System.Windows.Forms.Label();
+            this.NotifPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TitleLbl
+            // 
+            this.TitleLbl.AutoSize = true;
+            this.TitleLbl.BackColor = System.Drawing.Color.Transparent;
+            this.TitleLbl.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TitleLbl.ForeColor = System.Drawing.Color.White;
+            this.TitleLbl.Location = new System.Drawing.Point(12, 48);
+            this.TitleLbl.Name = "TitleLbl";
+            this.TitleLbl.Size = new System.Drawing.Size(0, 32);
+            this.TitleLbl.TabIndex = 0;
+            // 
+            // TrashBttn
+            // 
+            this.TrashBttn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(166)))), ((int)(((byte)(247)))));
+            this.TrashBttn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(166)))), ((int)(((byte)(247)))));
+            this.TrashBttn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TrashBttn.Location = new System.Drawing.Point(790, 17);
+            this.TrashBttn.Name = "TrashBttn";
+            this.TrashBttn.Size = new System.Drawing.Size(112, 81);
+            this.TrashBttn.TabIndex = 1;
+            this.TrashBttn.UseVisualStyleBackColor = false;
+            this.TrashBttn.Click += new System.EventHandler(this.TrashBttn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(166)))), ((int)(((byte)(247)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(806, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(82, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // TimeLbl
+            // 
+            this.TimeLbl.AutoSize = true;
+            this.TimeLbl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(166)))), ((int)(((byte)(247)))));
+            this.TimeLbl.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TimeLbl.ForeColor = System.Drawing.Color.White;
+            this.TimeLbl.Location = new System.Drawing.Point(588, 45);
+            this.TimeLbl.Name = "TimeLbl";
+            this.TimeLbl.Size = new System.Drawing.Size(34, 28);
+            this.TimeLbl.TabIndex = 3;
+            this.TimeLbl.Text = "ee";
+            // 
+            // NotifPanel
+            // 
+            this.NotifPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(166)))), ((int)(((byte)(247)))));
+            this.NotifPanel.Location = new System.Drawing.Point(5, 4);
+            this.NotifPanel.Name = "NotifPanel";
+            this.NotifPanel.Size = new System.Drawing.Size(904, 100);
+            this.NotifPanel.TabIndex = 4;
+            // 
+            // SingleNotification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(914, 109);
+            this.Controls.Add(this.TimeLbl);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.TrashBttn);
+            this.Controls.Add(this.TitleLbl);
+            this.Controls.Add(this.NotifPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(914, 200);
+            this.Name = "SingleNotification";
+            this.Text = "SingleNotification";
+            this.Load += new System.EventHandler(this.SingleNotification_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label TitleLbl;
+        private Button TrashBttn;
+        private PictureBox pictureBox1;
+        private Label TimeLbl;
+        private Panel NotifPanel;
+    }
+}
